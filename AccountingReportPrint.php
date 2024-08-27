495527d516036902b65ed653fbe657c4647a750d
Edward Thomas <159745579+edward7770@users.noreply.github.com>
2024-08-27 21:07:04 +0800

    Update AccountingReportPrint.php

--- AccountingReportPrint.php
+++ AccountingReportPrint.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <!--<title>Kanakku - Bootstrap Admin HTML Template</title>  -->
-
     <!-- Favicon -->
     <link rel="shortcut icon" href="assets/img/favicon.png">
 
